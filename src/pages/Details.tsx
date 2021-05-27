514171b70e08ec8096721cf9c223de47847ad7ff
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-27 16:50:34 +0200

    adds detail page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import { useParams } from 'react-router';
+import { ITrack } from '../types/deezer';
+
+type MusicParams = {
+  id: string;
+};
 const Details = () => {
-  return <h1>Details</h1>;
+  const { id } = useParams<MusicParams>();
+  const [track, setTrack] = useState<ITrack>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await fetch(
+          `${process.env.REACT_APP_API_URL}/track/${id}`
+        );
+        const data = await resp.json();
+        console.log(data);
+        setTrack(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [id]);
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Image rounded alt='albumCover' src={track?.album.cover_xl} />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Details;
